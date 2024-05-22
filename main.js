680e7fa9a685e78a7aeb6f2c482c9a03ef70e397
hpham474 <hpham474@gmail.com>
2024-05-21 23:18:59 -0500

    add borders to all boxes in grid. Have grid boxes change depending on amount of grids

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ function createGrid(size) {
     let grid = document.querySelector(".grid");
     for(let i = 0; i < size; i++) {
         let row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.height = `${100/gridSize}%`;
         for(let j = 0; j < size; j++) {
             let col = document.createElement("div");
+            col.style.width = `${100/gridSize}%`;
+            col.style.border = "1px solid grey";
             row.appendChild(col);
         }
         grid.appendChild(row);
     }
 }
+
+const gridSize = 16;
 
 createGrid(16);
