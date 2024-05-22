bc137ec70ceabe652ea53510d8704c973e8f1c2a
hpham474 <hpham474@gmail.com>
2024-05-21 23:31:51 -0500

    add event listener that changes divs to random color when hovered

--- main.js
+++ main.js
@@ -8,10 +8,24 @@ function createGrid(size) {
             let col = document.createElement("div");
             col.style.width = `${100/gridSize}%`;
             col.style.border = "1px solid grey";
+
+            col.addEventListener("mouseenter", () => {
+                col.style.backgroundColor = getRandomColor();
+            });
+
             row.appendChild(col);
         }
         grid.appendChild(row);
     }
+}
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() *16)];
+    }
+    return color;
 }
 
 const gridSize = 16;
